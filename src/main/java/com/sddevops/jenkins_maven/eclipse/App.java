7f1ce3074bf7d748675dc6b3f2477b70252d5efe
Eric Leow <ericleowyuquan@gmail.com>
2025-07-24 20:35:50 +0800

    feat: Add new print statement

--- src/main/java/com/sddevops/jenkins_maven/eclipse/App.java
+++ src/main/java/com/sddevops/jenkins_maven/eclipse/App.java
@@ -6,5 +6,6 @@ package com.sddevops.jenkins_maven.eclipse;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Worlds!");
+        System.out.println("Test");
     }
 }
